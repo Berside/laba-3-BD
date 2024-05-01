e2b3399b2b04aca50572040d650258a275060221
Berside <144826358+Berside@users.noreply.github.com>
2024-05-01 19:16:15 +0300

    version 0.2
    Fully working pages with the task from the first laboratory work and the second

--- бд-3 лаба/Приложение/server.js
+++ бд-3 лаба/Приложение/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql2 = require('mysql2/promise');
 const path = require('path');
-
+const bodyParser = require('body-parser');
 const pool = mysql2.createPool({
     host: 'localhost',
     user: 'root',
@@ -11,10 +11,13 @@ const pool = mysql2.createPool({
 
 const app = express();
 app.use(express.static(path.join(__dirname)));
+app.use(bodyParser.json());
+
 
 app.get('/users', function(req, res) {
     pool.query('SELECT * FROM user').then(function(data) {
         res.json(data);
+        console.log(data);
     });
 });
 
@@ -34,6 +37,10 @@ app.get('/main', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/log', function(req, res) {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
+
 app.get('/index', function(req, res) {
     res.sendFile(path.join(__dirname, 'new.html'));
 });
@@ -43,21 +50,40 @@ app.get('/employee', function(req, res) {
         res.json(data);
     });
 });
-app.post('/employee', (req, res) => {
-    res.json({ message: 'Success' });
+
+
+app.post('/employee', async (req, res) => {
+    const {Employee, lastname_Employee, name_Employee, middlename_Employee, post_Employee, division_Employee, date_of_admission } = req.body;
+
+    try {
+        const [result] = await pool.query(
+            `INSERT INTO employee (Employee, lastname_Employee, name_Employee, middlename_Employee, post_Employee, division_Employee, date_of_admission) VALUES (?,?,?,?,?,?,?)`,
+            [Employee, lastname_Employee, name_Employee, middlename_Employee, post_Employee, division_Employee, date_of_admission]
+        );
+        console.log(result);
+        res.send('Employee added successfully.');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('An error occurred while adding the employee.');
+    }
 });
 
-app.get('/gift', function(req, res) {
-    pool.query('SELECT * FROM gift').then(function(data) {
-        res.json(data);
-    });
+app.delete('/employee/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const [result] = await pool.query(
+            `DELETE FROM employee WHERE Employee =?`,
+            [id]
+        );
+        console.log(result);
+        res.send('Employee deleted successfully.');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('An error occurred while deleting the employee.');
+    }
 });
 
-app.get('/child', function(req, res) {
-    pool.query('SELECT * FROM child').then(function(data) {
-        res.json(data);
-    });
-});
 app.listen(3000, function() {
     console.log('server started!');
 });
